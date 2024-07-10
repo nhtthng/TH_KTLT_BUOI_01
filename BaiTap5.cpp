777fb8e154213ef35bf75ee7c92bf61dfd1ebd1a
nhat thang <nhtthng2812@gmail.com>
2024-07-10 15:32:11 +0700

    Add project files.

---
+++ BaiTap5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "Nhap b: ";
+	cin >> b;
+	cout << "Nhap c: ";
+	cin >> c;
+	cout << "Ket qua: " << c / (a - b);
+}
